9304387055dd893240b48ad0a6eaad451eeb78ef
ChavakulaNagasrimayi <113960939+ChavakulaNagasrimayi@users.noreply.github.com>
2022-09-20 11:25:40 +0530

    Add files via upload

---
+++ seasons.py
@@ -0,0 +1,10 @@
+a=input()
+b=int(input())
+if(a=="Mar" and b==20):
+    print("Summer")
+elif(a=="Jun" and b==21):
+    print("Spring")
+elif(a=="Sep" and b==22):
+    print("Fall")
+elif(a=="Dec" and b==21):
+    print("Winter")
